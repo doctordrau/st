353d2c7e9f81ccc21165659c214afa3016ae10e8
doctordrau <doctordrau@yahoo.com>
2017-10-08 00:51:38 +0300

    Update config.h

--- config.h
+++ config.h
@@ -107,7 +107,7 @@ static unsigned int defaultrcs = 257;
  * 6: Bar ("|")
  * 7: Snowman ("☃")
  */
-static unsigned int cursorshape = 2;
+static unsigned int cursorshape = 4;
 
 /*
  * Default colour and shape of the mouse cursor
