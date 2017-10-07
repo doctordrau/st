aeb9cbb4ab17b8081c14ab359130cb57172e54ef
doctordrau <doctordrau@yahoo.com>
2017-10-08 01:42:01 +0300

    Update config.h

--- config.h
+++ config.h
@@ -41,7 +41,7 @@ static unsigned int actionfps = 30;
  * blinking timeout (set to 0 to disable blinking) for the terminal blinking
  * attribute.
  */
-static unsigned int blinktimeout = 400;
+static unsigned int blinktimeout = 800;
 
 /*
  * thickness of underline and bar cursors
